c3123dbcca7710e7899a35b294492bca436cbe66
Lee Acton <leeacton@0x151.com>
2022-04-08 10:10:46 -0700

    Remove default values from Si4703::Si4703() implementation (Only valid on declaration).

--- src/Si4703.cpp
+++ src/Si4703.cpp
@@ -12,26 +12,26 @@
 //-----------------------------------------------------------------------------------------------------------------------------------
 Si4703::Si4703( 
                 // MCU Pins Selection
-                int rstPin    = 4,            // Reset Pin
-			          int sdioPin   = A4,           // I2C Data IO Pin
-			          int sclkPin   = A5,           // I2C Clock Pin
-			          int intPin    = 0,	          // Seek/Tune Complete and RDS interrupt Pin
+                int rstPin,          // = 4,            // Reset Pin
+			          int sdioPin,   // = A4,           // I2C Data IO Pin
+			          int sclkPin,   // = A5,           // I2C Clock Pin
+			          int intPin i,  // = 0,	          // Seek/Tune Complete and RDS interrupt Pin
 
                 // Band Settings
-			          int band      = BAND_US_EU,	  // Band Range
-                int space     = SPACE_100KHz,	// Band Spacing
-                int de        = DE_75us,		  // De-Emphasis
+			          int band,      // = BAND_US_EU,	  // Band Range
+                int space,           // = SPACE_100KHz,	// Band Spacing
+                int de,              // = DE_75us,		  // De-Emphasis
                 
                 // RDS Settings
 			// TODO:
                 // Tune Settings
 			// TODO:
                 // Seek Settings
-			          int skmode    = SKMODE_STOP,	// Seek Mode
-			          int seekth    = 24,	          // Seek Threshold
-			          int skcnt 	  = SKSNR_MAX,    // Seek Clicks Number Threshold
-			          int sksnr		  = SKCNT_MIN,    // Seek Signal/Noise Ratio
-                int agcd	    = 0				      // AGC disable
+			          int skmode,     // = SKMODE_STOP,	// Seek Mode
+			          int seekth,     // = 24,	          // Seek Threshold
+			          int skcnt,      // = SKSNR_MAX,    // Seek Clicks Number Threshold
+			          int sksnr,      // = SKCNT_MIN,    // Seek Signal/Noise Ratio
+                int agcd              // = 0				      // AGC disable
               )
 {
   // MCU Pins Selection
